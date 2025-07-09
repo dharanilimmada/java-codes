43cf1b07dae7cad8955ca5ff8df3b6379bb158dc
Dharani Limmada <105279279+dharanilimmada@users.noreply.github.com>
2025-07-09 20:51:41 +0530

    Add files via upload

---
+++ Implicit.java
@@ -0,0 +1,17 @@
+class  Implicit{
+    public static void main(String [] args){
+        float f = 2e4f;
+        double i = f;
+        float f2 = (float)f;
+          System.out.println(f);
+          System.out.println(i);
+          System.out.println(f2);
+  
+           
+
+
+       
+    }
+}
+                                                                                           
+                                                                                                                                                                                                                                           
