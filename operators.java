9cbf8fbc7c29ee723efb5db27c271966040684c5
Dharani Limmada <105279279+dharanilimmada@users.noreply.github.com>
2025-07-09 20:55:06 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,21 @@
+class Demo{
+      public static void main(String [] args){
+           int a = 10;
+           int b = 100;
+           b += 20;
+           a = a + 10;
+           a = a * 10;
+           b = b % 5;
+           b = b / 2;
+          
+          
+      
+           System.out.println(a);
+           System.out.println(b);
+           System.out.println(a++);
+           System.out.println(++a);
+           System.out.println(b--);
+           System.out.println(--b);
+      }
+
+}
