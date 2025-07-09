d8bab3d2204da7b4ab5054b23c131db867db4c2c
Dharani Limmada <105279279+dharanilimmada@users.noreply.github.com>
2025-07-09 21:02:06 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,19 @@
+class TypeConversion{
+
+    public static void main(String [] args){
+      int i = 113456789;
+      long l = i;
+      float f = i;
+      double d = i;
+      byte b = (byte) i ;
+      short s = (short) i ;
+       System.out.println("int "+i);
+       System.out.println("float "+f);
+       System.out.println("long "+f);
+       System.out.println("double "+d);
+       System.out.println("byte "+b);
+       System.out.println("short "+s);
+
+
+      }
+}
